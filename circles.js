a2dc8dc642978fbb706b69c1b403390b7cead1b7
Marc Micuta <mmicuta@gmail.com>
2014-10-26 23:01:10 +1100

    Fixed mouse event
    Now the canvas is no longer sized or centred correctly, need to look at
    a way to do this.

--- circles.js
+++ circles.js
@@ -22,35 +22,14 @@ paper.install(window);
 
 //Functions
 function circles() {
-	// Set up canvas and paper.js project
+	// Set up canvas and dimension based on window size
 	var canvas = document.getElementById("canvas01"); 
-	paper.setup(canvas);
-
+	
 	// Declare input variables
 	var n = $("#slider").slider("value");
 
-	// Declare objects
-	var circle = []
-
-	//UI Effects
-	//Mouse Hover Effect
-
-	var tool = new Tool();
-
-	
-
-
-	
 	canvasHeight = $(document).height();
 	canvasWidth = $(document).width();
-	//canvasHeight = window.innerHeight;
-	//canvasWidth = window.innerWidth;
-	//canvasWidth = $(canvas).outerWidth();
-
-	//function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	//paper.view.setViewSize(canvasWidth, canvasHeight)
-	//}
 
 	// Define height and width divisions by screen proportions
 	if (canvasHeight >= canvasWidth) {
@@ -65,7 +44,6 @@ function circles() {
 		canvasWidth = nw*gw;
 	}
 
-
 	var h1 = canvasHeight;
 	var w1 = canvasWidth;
 	var h2 = canvasHeight - gw;
@@ -74,12 +52,13 @@ function circles() {
 	//var radius = (0.618*gw)/2;
 	var radius = (0.8*gw)/2;
 
-	var testData = []
-
 	// Adjust canvas size to window size and centre it
 	canvas.height = h2;
 	canvas.width = w2;
 	canvas.style.margin = "0 auto";
+
+	// Setup Paper.js project
+	paper.setup(canvas);
 
 	// Create centroid point coordinates
 	var centroidsArray = []
@@ -102,31 +81,28 @@ function circles() {
 		//circle.strokeWidth = 1;
 		if (centroidsArray[i].state = Math.floor(Math.random()*2)) {
 			circle.fillColor = "black"
+			circle.strokeColor = "black";
+			circle.strokeWidth = 1
 		} else {
+			circle.fillColor = "white"
 			circle.strokeColor = "black";
 			circle.strokeWidth = 1
 		}
+
 		circle.onMouseEnter = function(event){
-			this.fillColor = "hsla(0,0%,40%,1)";
-			//this.fillColor = "red";
+			this.fillColor = "hsla(0,0%,50%,1)";
 		};
 
 		circle.onMouseLeave = function(event){
 			this.fillColor = "black";
-			//this.fillColor = "red";
 		};			
 	};
 
 
-	
-
 
 	paper.view.draw();
 
-	//document.getElementById("test").innerText=$("#slider").slider("value");
-	//document.getElementById("test").innerText=centroidsArray.length;
 };
-
 
 
 //Setup SVG Canvas
@@ -138,6 +114,3 @@ $(document).ready(function() {
 $(window).resize(function() {
 	circles();
 });
-
-//Data
-
